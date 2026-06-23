18cf61c11f699a19ea0d41c0e0d8eb3c222f212f
muhrasyadan-spec <muhrasyadan@gmail.com>
2026-06-23 16:56:00 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+const form = document.getElementById("sellerForm");
+const table = document.getElementById("sellerTable");
+
+let sellers = JSON.parse(localStorage.getItem("sellers")) || [];
+
+renderData();
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const seller = {
+        nama: document.getElementById("nama").value,
+        hp: document.getElementById("hp").value,
+        alamat: document.getElementById("alamat").value,
+        minuman: document.getElementById("minuman").value
+    };
+
+    sellers.push(seller);
+
+    localStorage.setItem(
+        "sellers",
+        JSON.stringify(sellers)
+    );
+
+    form.reset();
+
+    renderData();
+});
+
+function renderData(){
+    table.innerHTML = "";
+
+    sellers.forEach((seller,index)=>{
+
+        table.innerHTML += `
+        <tr>
+            <td>${seller.nama}</td>
+            <td>${seller.hp}</td>
+            <td>${seller.alamat}</td>
+            <td>${seller.minuman}</td>
+            <td>
+                <button
+                    class="delete-btn"
+                    onclick="hapusData(${index})">
+                    Hapus
+                </button>
+            </td>
+        </tr>
+        `;
+    });
+}
+
+function hapusData(index){
+    sellers.splice(index,1);
+
+    localStorage.setItem(
+        "sellers",
+        JSON.stringify(sellers)
+    );
+
+    renderData();
+}
